da350c2b611afaec88d0a60134b26c625ed45423
John T. Alexander <jalexander.qed@gmail.com>
2017-02-27 14:37:44 -0800

    Removed build folder

--- john_alexander_proj5/Glitter/Build/Glitter/Vendor/assimp/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/jalexander/git/cs280_repo/john_alexander_proj5/Glitter/Glitter/Vendor/assimp/contrib/zlib
-# Build directory: C:/Users/jalexander/git/cs280_repo/john_alexander_proj5/Glitter/Build/Glitter/Vendor/assimp/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
